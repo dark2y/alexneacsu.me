75614297f48df856b266bc91da39dadfe670dac2
Alexandru Neacsu <alexandru.neacsu@cybertech.ro>
2025-06-07 09:35:13 +0300

    Local changes before deployment

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 git add .
 git commit -m "Local changes before deployment"
-git pull origin master
+git push origin master
 
 git checkout gh-pages
 git merge --no-ff master
 git push origin gh-pages
+
+git checkout master
